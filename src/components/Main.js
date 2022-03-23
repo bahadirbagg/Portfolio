aa8dea373f96078bd212b0c54120ed980ae71b15
bahadirbagg <bahadirbagg@gmail.com>
2022-03-23 09:15:29 +0300

    Initial commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,10 @@
 import React from "react";
 import SmoothList from "react-smooth-list";
 import Particless from "./Particless";
-import { useAnimation } from "react-rebound";
 import { projects } from "../project";
 
 function Main() {
-  const [hovered, setHovered] = React.useState(false);
 
-  
-  const ref = React.useRef();
-  useAnimation(ref, {scaleX: hovered ? 1.1 : 1, scaleY: hovered ? 1.1 : 1 });
-  
-  let astyle={
-    color:''
-  }
-  if(hovered){
-    astyle.color="#66fcf1";
-  }
     
   return (
     <div>
@@ -30,8 +18,8 @@ function Main() {
         </SmoothList>
       </section>
       <section id="about">
-        <h2 className="heading" data-aos="fade-up">About Me</h2>
-        <p data-aos="fade-up">
+        <h2 className="heading" data-aos="fade-up" data-aos-anchor-placement="center-center">About Me</h2>
+        <p data-aos="fade-up" data-aos-anchor-placement="center-center">
           Hello! My name is Bahadır and I enjoy creating things that live on the
           internet. My interest in web development started back in 2021.I’m a
           Full-Stack Developer located in Turkey. I have a serious passion for
@@ -39,23 +27,19 @@ function Main() {
         </p>
       </section>
       <section id="projects">
-        <h2 className="heading">Projects</h2>
+        <h2 className="heading" data-aos="fade-up" data-aos-anchor-placement="center-center">Projects</h2>
         <ul className="projects-background">
           {projects.map((proje) => {
             return (
                 <li className="project-li" data-aos="fade-up" data-aos-anchor-placement="center-center">
                     <div
                         className="project-inner"
-                        ref={ref}
-                        onMouseOver={() => setHovered(true)}
-                        onMouseOut={() => setHovered(false)}
                     >   
                         <div class="project-top">
                             <i class="fa-regular fa-folder"></i>
                         </div>
                         <h3 className="project-title">
                             <a
-                                style={astyle}
                                 href={proje.href}
                                 target="_blank"
                                 rel="noopener noreferrer"
